6dc3cac4c026d5e40fe62c9672fcb406ab52efb1
jfle0011 <108248374+jfle0011@users.noreply.github.com>
2022-10-09 15:10:21 +1100

    Add files via upload

--- map_vis.js
+++ map_vis.js
@@ -2,3 +2,8 @@ var vg_1 = "co2_map.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 
 }).catch(console.error);
+
+var vg_2 = "temperature_line.json";
+vegaEmbed("#tempemm", vg_2).then(function(result) {
+
+}).catch(console.error);
