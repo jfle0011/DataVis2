6d4854e77cb580ff2f90d8ae106eef169680dfe7
jfle0011 <108248374+jfle0011@users.noreply.github.com>
2022-10-17 19:01:01 +1100

    Add files via upload

--- map_vis.js
+++ map_vis.js
@@ -1,19 +1,19 @@
 var vg_1 = "co2_map.json";
-vegaEmbed("#map", vg_1).then(function(result) {
+vegaEmbed("#map", vg_1, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var vg_2 = "temperature_line.json";
-vegaEmbed("#tempemm", vg_2).then(function(result) {
+vegaEmbed("#tempemm", vg_2, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var vg_3 = "energy_mix2019.json";
-vegaEmbed("#enmix", vg_3).then(function(result) {
+vegaEmbed("#enmix", vg_3, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var vg_4 = "target.json";
-vegaEmbed("#status", vg_4).then(function(result) {
+vegaEmbed("#status", vg_4, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
